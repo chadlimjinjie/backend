d606bfb372dfab6de8c8c989aaa8010b4c21ca59
chadlim <chadlimjinjie@gmail.com>
2022-08-13 15:09:06 +0800

    Added CORS middleware

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers.v1 import disney_plus
 from routers.v1 import platform_crowd_level as pcl
@@ -13,8 +14,22 @@ from routers.v1 import youtube
 
 HOST = "0.0.0.0"
 PORT = os.getenv("PORT") or 8080
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 apiv1 = FastAPI()
